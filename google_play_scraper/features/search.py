3d0d6a462b5f4325deaa2b7578eb9b23f529b657
smit-kukadiya <smitk85143@gmail.com>
2024-01-11 15:40:34 +0530

    Add search result details to search function

--- google_play_scraper/features/search.py
+++ google_play_scraper/features/search.py
@@ -1,12 +1,14 @@
 import json
 from typing import Any, Dict, List
 from urllib.parse import quote
+import re
 
 from google_play_scraper.constants.element import ElementSpecs
 from google_play_scraper.constants.regex import Regex
 from google_play_scraper.constants.request import Formats
 from google_play_scraper.exceptions import NotFoundError
 from google_play_scraper.utils.request import get
+from google_play_scraper.features.app import app as app_data
 
 
 def search(
@@ -46,6 +48,32 @@ def search(
 
     n_apps = min(len(dataset), n_hits)
     search_results = []
+    try:
+        long_string = matches[7][1300:1500]
+        app_id_pattern = re.compile(r'https://play.google.com/store/apps/details([^"&]+)')
+
+        # Find all matches in the string
+        main_app = app_id_pattern.findall(long_string)[0].split("?id\\u003d")[1]
+        app_result = app_data(main_app, lang=lang, country=country)
+        app = {}
+        app['trackCensoredName'] = main_app
+        app['icon'] = app_result['icon']
+        app['screenshots'] = app_result['screenshots']
+        app['title'] = app_result['trackCensoredName']
+        app['averageUserRating'] = app_result['averageUserRating']
+        app['genre'] = app_result['genre']
+        app['price'] = app_result['price']
+        app['free'] = app_result['free']
+        app['currency'] = app_result['currency']
+        app['video'] = app_result['video']
+        app['videoImage'] = app_result['videoImage']
+        app['description'] = app_result['description']
+        app['descriptionHTML'] = app_result['descriptionHTML']
+        app['sellerName'] = app_result['sellerName']
+        app['installs'] = app_result['installs']
+        search_results.append(app)
+    except:
+        pass
     for app_idx in range(n_apps):
         app = {}
         for k, spec in ElementSpecs.Searchresult.items():

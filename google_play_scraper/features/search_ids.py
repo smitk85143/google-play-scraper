8f52699fbd5368efa6ff1b3e3c0f2984317bcda4
smit-kukadiya <smitk85143@gmail.com>
2024-01-17 11:08:49 +0530

    Add herder and timeout parameters to search_ids function

--- google_play_scraper/features/search_ids.py
+++ google_play_scraper/features/search_ids.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List
 from google_play_scraper.features.search import search
 
 def search_ids(
-    query: str, n_hits: int = 50, lang: str = "en", country: str = "us"
+    query: str, n_hits: int = 50, lang: str = "en", country: str = "us", herder: dict = None, timeout: int = 2
 ) -> List[Dict[str, Any]]:
     results = search(query, n_hits, lang, country)
     return [result["trackCensoredName"] for result in results]
